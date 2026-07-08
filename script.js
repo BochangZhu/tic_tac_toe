a4ce6176cb733ac0f80fbe731a78315118f8f8da
BochangZhu <160828488+BochangZhu@users.noreply.github.com>
2026-07-07 21:00:05 -0700

    feat: createPlayer object & domHandler obj with init property

--- script.js
+++ script.js
@@ -60,5 +60,93 @@ const gameBoard = (() => {
         boardMatrix[row][col] = itemCode;
     }
 
+    return {resetBoard, logBoard, decideGameState, placeItem};
 
-})
+})();
+
+
+const createPlayer = ((name) => {
+    let score = 0;
+    
+    const scoreIncrement = () => score += 1;
+
+    const scoreReset = () => score = 0;
+
+    return {scoreIncrement, scoreReset, name};
+
+})();
+
+
+const domHandler = (() => {
+
+    // create dialog template
+    const dialog = document.createElement("dialog");
+
+    const para = document.createElement("div");
+    para.className = "para";
+
+    const btns = document.createElement("div");
+    btns.className = "btns";
+
+    const btn1 = document.createElement("button");
+    const btn2 = document.createElement("button");
+
+    btns.append(btn1, btn2);
+
+    dialog.append(para, btns);
+
+
+    
+    const domInit = () => {
+        // create headerText and main
+        const header = document.createElement("h1");
+        header.textContent = "TIC TAC TOE";
+        header.className = "gameHeader";
+
+        const mainPanel = document.createElement("div");
+        mainPanel.className = "mainPanel";
+
+        // init the dialog
+        para.textContent = "Welcome to the Tic Tac Toe Game! Have you played it before?";
+
+        // btn2 
+        // redirects to yt tutorial
+        btn2.textContent("No!");
+        const link2YT = document.createElement("a");
+
+        link2YT.setAttribute("href", "https://youtu.be/3qzcAMShotQ?si=c4J5aVYoWZtCH1mq");
+        link2YT.setAttribute("target", "_blank");
+        link2YT.setAttribute("rel", "noopener");
+
+        link2YT.addEventListener("click", ()=>{
+            btn2.setAttribute("title", "You watched the tutorial didn't you?");
+            btn2.disabled = true;
+        })
+
+        btn2.appendChild(link2YT);
+
+
+        // btn1
+        btn1.textContent = "Of Course...";
+
+
+        document.body.append(header, mainPanel, dialog);
+
+        dialog.showModal();
+
+    }
+
+    // const boardUpdate = () => {
+
+    // }
+
+    return {domInit};
+
+
+})();
+
+const gameManager = (() => {
+    let whosTurn = 0;
+
+
+})();
